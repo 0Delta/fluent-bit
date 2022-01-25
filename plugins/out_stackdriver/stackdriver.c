958803a35f499dd23fa0ead3a58feb49fe5b0c98
0Delta <0deltast@gmail.com>
2022-01-25 20:08:43 +0900

    [wip] test

--- plugins/out_stackdriver/stackdriver.c
+++ plugins/out_stackdriver/stackdriver.c
@@ -1467,8 +1467,8 @@ static flb_sds_t stackdriver_format(struct flb_stackdriver *ctx,
     const char *new_trace;
 
     /* Parameters for spanID */
-    int spanid_extracted = FLB_FALSE;
-    flb_sds_t spanid;
+    int span_id_extracted = FLB_FALSE;
+    flb_sds_t span_id;
 
     /* Parameters for log name */
     int log_name_extracted = FLB_FALSE;
@@ -1843,11 +1843,11 @@ static flb_sds_t stackdriver_format(struct flb_stackdriver *ctx,
             entry_size += 1;
         }
 
-        /* Extract spanid */
-        spanid_extracted = FLB_FALSE;
+        /* Extract spanID */
+        span_id_extracted = FLB_FALSE;
         if (ctx->span_id_key
-            && get_string(&spanid, obj, ctx->span_id_key) == 0) {
-            spanid_extracted = FLB_TRUE;
+            && get_string(&span_id, obj, ctx->span_id_key) == 0) {
+            span_id_extracted = FLB_TRUE;
             entry_size += 1;
         }
 
@@ -1955,14 +1955,14 @@ static flb_sds_t stackdriver_format(struct flb_stackdriver *ctx,
             flb_sds_destroy(trace);
         }
 
-        /* Add spanId into the log entry */
-        if (spanid_extracted == FLB_TRUE) {
-            msgpack_pack_str(&mp_pck, 6);
-            msgpack_pack_str_body(&mp_pck, "spanId", 6);
-            len = flb_sds_len(spanid);
+        /* Add spanID into the log entry */
+        if (span_id_extracted == FLB_TRUE) {
+            msgpack_pack_str(&mp_pck, 7);
+            msgpack_pack_str_body(&mp_pck, "span_id", 7);
+            len = flb_sds_len(span_id);
             msgpack_pack_str(&mp_pck, len);
-            msgpack_pack_str_body(&mp_pck, spanid, len);
-            flb_sds_destroy(spanid);
+            msgpack_pack_str_body(&mp_pck, span_id, len);
+            flb_sds_destroy(span_id);
         }
 
         /* Add insertId field into the log entry */
